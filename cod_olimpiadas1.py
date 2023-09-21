ce4e8aa1cf8632fdcc1213aaf4897a264f1261b3
Otero1407 <145025813+Otero1407@users.noreply.github.com>
2023-09-21 08:05:12 -0600

    Create cod_olimpiadas1.py

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,12 @@
+def mult(a,b):
+  x = a*b
+  return x
+
+  def division(a,b):
+    x= a/b
+    return x
+    
+  a=input(float("inserte el primer numero"))
+  b=input(float("inserte el segundo numero"))
+
+print("la multiplicación da", mult(a,b), "la división da", division(a,b))
