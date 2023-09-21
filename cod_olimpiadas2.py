400e3c354ef4b7f306a448ef5b8e081c4d1ad464
Otero1407 <145025813+Otero1407@users.noreply.github.com>
2023-09-21 08:07:07 -0600

    Create cod_olimpiadas2.py

---
+++ cod_olimpiadas2.py
@@ -0,0 +1,15 @@
+def suma(a,b):
+  x = a+b
+  return x
+
+def resta(a,b):
+    x= a-b
+    return x
+    
+print("inserte el primer numero")
+a=int(input())
+  
+print("inserte el segundo numero")
+b=int(input())
+
+print("la suma da", suma(a,b), "la resta da", resta(a,b))
